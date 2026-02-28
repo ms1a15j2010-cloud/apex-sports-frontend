155027f1548d46b4bb82b4955dabbcdf9220d169
Your Full Name <alikhan@gmail.com>
2026-02-28 10:19:46 +0500

    convert next config to ES module

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 const nextConfig = {
-  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/data",
+        headers: [
+          {
+            key: "Vary",
+            value: "X-Vercel-IP-Country",
+          },
+          {
+            key: "Cache-Control",
+            value: "s-maxage=3600",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
