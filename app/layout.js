4beed50fb609a0be3b95b57718737283d2bba5a4
Your Full Name <alikhan@gmail.com>
2026-03-13 12:05:34 +0500

    add google verification meta

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,20 @@
 import "./globals.css";
-
+import Head from "next/head";
 export const metadata = {
   title: "Live Football Scores Today | Apex Sports",
   description: "Watch live football scores with real-time updates.",
-  verification: {
-    google: "eMzm74d1mP9oknPY4pOLjBVGTiPhSvC3zomVjAL7J1k",
-  },
+  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <Head>
+        <meta
+          name="google-site-verification"
+          content="eMzm74d1mP9oknPY4pOLjBVGTiPhSvC3zomVjAL7J1k"
+        />
+      </Head>
       <body style={{ margin: 0, background: "#020617", color: "#fff" }}>
         {children}
 
