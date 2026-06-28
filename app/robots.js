2c15dd6587b9915e4e677d06bd4962cebc211a5d
Your Full Name <alikhan@gmail.com>
2026-06-28 11:17:14 +0500

    Add correct robots file

--- app/robots.js
+++ app/robots.js
@@ -1,23 +1,9 @@
-export default async function sitemap() {
-  const baseUrl = "https://apex-sports-frontend.vercel.app";
-
-  const staticRoutes = [
-    "",
-    "/league/epl",
-  ];
-
-  const matchIds = [1, 2]; // later you can fetch from backend
-
-  const matchRoutes = matchIds.map((id) => ({
-    url: `${baseUrl}/match/${id}`,
-    lastModified: new Date(),
-  }));
-
-  return [
-    ...staticRoutes.map((route) => ({
-      url: `${baseUrl}${route}`,
-      lastModified: new Date(),
-    })),
-    ...matchRoutes,
-  ];
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://apex-sports-frontend.vercel.app/sitemap.xml",
+  };
 }
