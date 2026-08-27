64d076fe7bf6202eb4510c0e3d25f4d16ba98044
Your Full Name <alikhan@gmail.com>
2026-08-27 11:31:55 +0500

    Add Pinterest domain verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -111,6 +111,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="p:domain_verify" content="8ba1cefbab1bcb1855dc60ef7ec80935"/>
     <script
       async
       src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1963005458652987"
